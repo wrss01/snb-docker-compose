3e395098021968ad88f01ebfe37a421835555c83
wrss01 <39665821+wrss01@users.noreply.github.com>
2025-02-25 11:43:03 +0800

    Update deploy-simple.sh
    更改容器版本和默认端口

--- deploy-simple.sh
+++ deploy-simple.sh
@@ -1,5 +1,5 @@
 # version must be >= 1.8.8
-export VERSION=1.8.8
-export EXPOSE_PORT=9900
+export VERSION=1.8.9
+export EXPOSE_PORT=80
 
 docker-compose --file ./docker-compose-simple.yml  --project-name snb up -d
