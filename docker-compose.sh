0bf437eaf46c9df8073fd5b6536951adf5bfef78
wrss01 <39665821+wrss01@users.noreply.github.com>
2025-03-31 09:57:06 +0800

    docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -9,4 +9,4 @@ chmod 777 data_minio
 
 chmod +x wait-for-it.sh 
 
-docker-compose --file ./docker-compose-simp.yml  --project-name snb up -d
+docker-compose --file ./docker-compose.yml  --project-name snb up -d
