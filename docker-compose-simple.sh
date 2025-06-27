971bd9c1bceb36eea84c002ddd4202c4254eb4a3
wrss01 <39665821+wrss01@users.noreply.github.com>
2025-06-27 10:38:12 +0800

    添加命令行参数,可以up\down\stop\start,默认是up -d

--- docker-compose-simple.sh
+++ docker-compose-simple.sh
@@ -1,5 +1,5 @@
 # version must be >= 1.8.8
 export VERSION=1.8.10
 export EXPOSE_PORT=80
-
-docker-compose --file ./docker-compose-simple.yml  --project-name snb up -d
+CMD="${1:-up -d}"
+docker-compose --file ./docker-compose-simple.yml  --project-name snb $CMD
