edeaaf815cf5fc14b2c99d3b429f89f5c9c22f80
wrss01 <39665821+wrss01@users.noreply.github.com>
2025-08-28 15:19:06 +0800

    docker compose 支持

--- docker-compose-simple.sh
+++ docker-compose-simple.sh
@@ -2,10 +2,9 @@
 export VERSION=1.8.10
 export EXPOSE_PORT=80
 CMD="${1:-up -d}"
-
-if command -v docker-compose &> /dev/null; then
+if command -v docker-compose > /dev/null ; then
     docker-compose --file ./docker-compose-simple.yml  --project-name snb $CMD
-elif docker compose version &> /dev/null; then
+elif docker compose version > /dev/null ; then
     docker compose --file ./docker-compose-simple.yml  --project-name snb $CMD
 else
     echo "错误: 未找到 docker-compose 或 docker compose，请先安装 Docker 及 Compose."
--- docker-compose.sh
+++ docker-compose.sh
@@ -9,9 +9,9 @@ chmod +x wait-for-it.sh
 
 CMD="${1:-up -d}"
 
-if command -v docker-compose &> /dev/null; then
+if command -v docker-compose > /dev/null ; then
     docker-compose --file ./docker-compose.yml  --project-name snb $CMD
-elif docker compose version &> /dev/null; then
+elif docker compose version > /dev/null ; then
     docker compose --file ./docker-compose.yml  --project-name snb $CMD
 else
     echo "错误: 未找到 docker-compose 或 docker compose，请先安装 Docker 及 Compose."
